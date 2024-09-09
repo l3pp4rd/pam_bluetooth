56759c69d913e0edad212bb9b2f98d8378fdee98
gedi <gediminas.morkevicius@gmail.com>
2024-09-09 10:36:28 +0300

    disconnect after succesful authentication

--- pam_bluetooth.c
+++ pam_bluetooth.c
@@ -13,14 +13,19 @@ PAM_EXTERN int pam_sm_acct_mgmt(pam_handle_t *pamh, int flags, int argc, const c
 }
 
 PAM_EXTERN int pam_sm_authenticate( pam_handle_t *pamh, int flags,int argc, const char **argv ) {
-  char* mac = ""; //enter mac address of device
+  char* mac = ""; // enter mac address of device, example: 80:82:2K:7D:B8:67
   char cmd[100] = "";
   snprintf(cmd, sizeof(cmd), "bluetoothctl connect %s > /dev/null 2>&1", mac);
   int status = system(cmd);
   if (status != 0) {
+    // if we are able to connect means the mobile is near the laptop
     return PAM_IGNORE;
+  } else {
+    // disconnect since do not want to use mobile audio on laptop
+    snprintf(cmd, sizeof(cmd), "bluetoothctl disconnect %s > /dev/null 2>&1", mac);
+    system(cmd);
+    return PAM_SUCCESS;
   }
-  return PAM_SUCCESS;
 }
 
 
